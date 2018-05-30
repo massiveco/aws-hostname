7309b54dc52feb6921b4d2f5a9e60ec15639b1f5
Dan Williams <me@deedubs.com>
2018-05-29 21:01:59 -0400

    Move GenerateHostname to its own package so it can be imported (#3)

---
+++ identity/main.go
@@ -0,0 +1,32 @@
+package identity
+
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+// GenerateHostname Generates a hostname from the ec2.Instance and tags
+func GenerateHostname(instance ec2.Instance) (*string, error) {
+
+	privateIP := instance.PrivateIpAddress
+	hashedPrivateIP := strings.Replace(*privateIP, ".", "-", 10)
+
+	tags := tagsToMap(instance.Tags)
+
+	hostname := tags["HostnamePrefix"] + hashedPrivateIP
+
+	return &hostname, nil
+}
+
+func tagsToMap(tags []*ec2.Tag) map[string]string {
+
+	tagMap := make(map[string]string)
+
+	for _, element := range tags {
+		var el = *element
+		tagMap[*el.Key] = *el.Value
+	}
+
+	return tagMap
+}
