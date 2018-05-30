53fa0d8c59ea099065e3cafcb9f13ef6e47c11d9
Dan Williams <me@deedubs.com>
2018-05-29 20:52:22 -0400

    Rewrite GenerateHostname to accept a `ec2.Instance` record (#1)

--- main.go
+++ main.go
@@ -15,25 +15,22 @@ import (
 
 const hostnamePath = "/etc/hostname"
 
-var prefixLookupName, ec2TagName string
+var ec2TagName string
 var setEc2Tag, writeToDisk, setHostname bool
 
 func main() {
-	flag.StringVar(&prefixLookupName, "prefix-lookup-name", "HostnamePrefix", "Set the Ec2 tag name to read the prefix from")
 	flag.BoolVar(&writeToDisk, "write-disk", true, "Instruct aws-hostname to write the generated hostname to disk")
 	flag.BoolVar(&setEc2Tag, "write-tag", true, "Instruct aws-hostname to write the generated hostname to an Ec2 instance tag")
 	flag.BoolVar(&setHostname, "apply", true, "Instruct aws-hostname to syscall.Sethostname")
 	flag.StringVar(&ec2TagName, "tag", "Name", "Which tag to write the hostname to")
 	flag.Parse()
 
-	meta := ec2metadata.New(session.New())
-
-	identity, err := meta.GetInstanceIdentityDocument()
+	instance, err := getInstance()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hostname, err := GenerateHostname(identity)
+	hostname, err := GenerateHostname(*instance)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,7 +47,7 @@ func main() {
 
 		input := &ec2.CreateTagsInput{
 			Resources: []*string{
-				aws.String(identity.InstanceID),
+				aws.String(*instance.InstanceId),
 			},
 			Tags: []*ec2.Tag{
 				{
@@ -77,42 +74,50 @@ func main() {
 	log.Printf("Set hostname to: %s", *hostname)
 }
 
-// GenerateHostname Generates a hostname from the instances identity and tags
-func GenerateHostname(identity ec2metadata.EC2InstanceIdentityDocument) (*string, error) {
+func getInstance() (*ec2.Instance, error) {
 
-	instanceID := identity.InstanceID
-	privateIP := identity.PrivateIP
-	hashedPrivateIP := strings.Replace(privateIP, ".", "-", 10)
+	meta := ec2metadata.New(session.New())
 
+	identity, err := meta.GetInstanceIdentityDocument()
+	if err != nil {
+		log.Fatal(err)
+	}
 	svc := ec2.New(session.New())
-	input := &ec2.DescribeTagsInput{
+	describedInstances, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
 				Name: aws.String("resource-id"),
 				Values: []*string{
-					aws.String(instanceID),
+					aws.String(identity.InstanceID),
 				},
 			},
 		},
+	})
+
+	if describedInstances.Reservations[0] != nil && describedInstances.Reservations[0].Instances[0] != nil {
+		return describedInstances.Reservations[0].Instances[0], nil
 	}
+	return nil, nil
+}
 
-	result, err := svc.DescribeTags(input)
-	if err != nil {
-		return nil, err
-	}
+// GenerateHostname Generates a hostname from the ec2.Instance and tags
+func GenerateHostname(instance ec2.Instance) (*string, error) {
 
-	tags := tagsToMap(*result)
+	privateIP := instance.PrivateIpAddress
+	hashedPrivateIP := strings.Replace(*privateIP, ".", "-", 10)
 
-	hostname := tags[prefixLookupName] + hashedPrivateIP
+	tags := tagsToMap(instance.Tags)
+
+	hostname := tags["HostnamePrefix"] + hashedPrivateIP
 
 	return &hostname, nil
 }
 
-func tagsToMap(tags ec2.DescribeTagsOutput) map[string]string {
+func tagsToMap(tags []*ec2.Tag) map[string]string {
 
 	tagMap := make(map[string]string)
 
-	for _, element := range tags.Tags {
+	for _, element := range tags {
 		var el = *element
 		tagMap[*el.Key] = *el.Value
 	}
